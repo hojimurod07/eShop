af5cefd3f886fb0f038a374b57be212d795211e8
hojimurod07 <hojimurad2007@gmail.com>
2024-02-23 10:40:04 +0500

    create category interface and respository

---
+++ eShop/Data/interfaces/ICategoryInterface.cs
@@ -0,0 +1,13 @@
+﻿using eShop.Data.Entites;
+
+namespace eShop.Data.interfaces
+{
+    public interface ICategoryInterface
+    {
+        List<Category> GetCategories();
+        Category GetCategoryByIdd(int id);
+        void AddCategory(Category category);
+        void UpdateCategory(Category category);
+        void DeleteCategory(int id);
+    }
+}

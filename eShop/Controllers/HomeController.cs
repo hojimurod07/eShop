f0e9f5745b95fbfec7d7b9108dc51066cbbaee2f
hojimurod07 <hojimurad2007@gmail.com>
2024-03-04 10:31:00 +0500

    indexviewmodel ishlamoqda

--- eShop/Controllers/HomeController.cs
+++ eShop/Controllers/HomeController.cs
@@ -2,15 +2,24 @@
 
 namespace eShop.Controllers
 {
-    public class HomeController(ICategoryService categoryService) : Controller
+    public class HomeController(ICategoryService categoryService,
+                                IProductService productService) : Controller
     {
 
         private readonly ICategoryService _categoryService = categoryService;
+        private readonly IProductService _productService = productService;
 
         public IActionResult Index()
         {
             var categories = _categoryService.GetAll();
-            return View(categories);
+            var products = _productService.GetAll();
+            IndeViewModel model = new IndeViewModel()
+            {
+                Categories = categories,
+                Products = products
+            };
+
+            return View(model);
         }
     }
 }
--- eShop/globalUsing.cs
+++ eShop/globalUsing.cs
@@ -5,3 +5,4 @@ global using eShop.BLL.Common;
 global using eShop.BLL.DTOs.CategoryDTOs;
 global using eShop.BLL.DTOs.ProductDTOs;
 global using eShop.BLL.Interfaces;
+global using eShop.Models;
